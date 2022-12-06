207544f74080aeff957e6dddfa9ab27e1b6890a5
Enrico Bandiera <e.bandiera@teamsystem.com>
2022-12-06 08:56:50 +0100

    feat: day6

--- src/Day6.hs
+++ src/Day6.hs
@@ -1,3 +1,17 @@
 module Main where
- 
-main = print "day6"
+
+import Util.File (readLines)
+import Data.List (nub)
+
+main :: IO ()
+main = do
+  lines <- readLines "data/day6/day6.txt"
+  print $ day6 4 <$> lines
+  print $ day6 14 <$> lines
+
+day6 :: Int -> String -> Int
+day6 o = go 1
+  where
+    go n xs
+      | (==o) . length . nub $ take o xs = n+o-1
+      | otherwise = go (n+1) (tail xs)
